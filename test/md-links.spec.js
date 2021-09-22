3d0e0fa5317d0a04630f93bb5c2ce1802d8d90fa
yesireth <yesireth88@gmail.com>
2021-09-22 13:05:25 -0500

    Se hicieron los test de las funciones creadas de la appi

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -4,4 +4,49 @@ describe('existsPath', () => {
   it('Deberia ser una función', () => {
     expect(typeof path.existsPath).toBe('function')
   })
+  it('Deberia retornar si existe la ruta ', () => {
+    expect(path.existsPath('C:\\Users\\51944\\laboratoria\\LIM015-md-links\\README.md')).toBe(true)
+  })
+  it('Deberia retornar que no existe la ruta', () => {
+    expect(path.existsPath('C:\\Users\\51944\\laboratoria\\LIM015-md-links\\READMEEEE.md')).toBe(false)
+  })
+})
+describe('pathAbsolute ', () => {
+  it('Deberia ser una función', () => {
+    expect(typeof path.pathAbsolute).toBe('function')
+  })
+  it('Deberia retornar si existe la ruta ', () => {
+    expect(path.pathAbsolute('C:\\Users\\51944\\laboratoria\\LIM015-md-links\\src\\mdlinks.md')).toBe(true)
+  })
+  it('Deberia retornar que no es absoluta la ruta', () => {
+    expect(path.existsPath('READMEEEE.md')).toBe(false)
+  })
+})
+describe('pathConvertToAbs ', () => {
+  it('Deberia ser una función', () => {
+    expect(typeof path.pathConvertToAbs).toBe('function')
+  })
+  it('Deberia convertir la ruta relativa en ruta absoluta', () => {
+    expect(path.pathConvertToAbs('README.md')).toBe('C:\\Users\\51944\\laboratoria\\LIM015-md-links\\README.md')
+  })
+})
+describe('patDirectory ', () => {
+  it('Deberia ser una función', () => {
+    expect(typeof path.patDirectory).toBe('function')
+  })
+  it('Deberia retonar true si es directorio', () => {
+    expect(path.patDirectory('C:\\Users\\51944\\laboratoria\\LIM015-md-links\\src')).toBe(true)
+  })
+})
+describe('readFileAndDirectory ', () => {
+  it('Deberia ser una función', () => {
+    expect(typeof path.readFileAndDirectory).toBe('function')
+  })
+  it('Deberia retonar un array de archivos md', () => {
+    const arrayMd = [
+      'C:\\Users\\51944\\laboratoria\\LIM015-md-links\\src\\archivo\\archivo 2\\archivo.md',
+      'C:\\Users\\51944\\laboratoria\\LIM015-md-links\\src\\archivo\\mdlinks.md'
+    ]
+    expect(path.readFileAndDirectory('C:\\Users\\51944\\laboratoria\\LIM015-md-links\\src')).toEqual(arrayMd)
+  })
 })
